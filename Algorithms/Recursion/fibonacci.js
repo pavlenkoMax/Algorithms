46d4a223a98d620dabc2f6416b31aaea01bd30f8
BH18308 <maksym.pavlenko@macys.com>
2022-10-25 15:19:45 +0300

    add fibbonachi memoization functions

--- Algorithms/Recursion/fibonacci.js
+++ Algorithms/Recursion/fibonacci.js
@@ -4,7 +4,6 @@
 // the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
 
 //For example: fibonacciRecursive(6) should return 8
-
 function fibonacciIterative(number){
   const resultArr = [];
 
@@ -21,8 +20,7 @@ function fibonacciIterative(number){
 
 console.log(fibonacciIterative(13));
 
-
-function fibonacciRecursive(number) {
+function fibonacciRecursive(number) { // O(2^n)
   if (number < 2) {
     return number;
   }
@@ -30,4 +28,34 @@ function fibonacciRecursive(number) {
   return fibonacciRecursive(number - 1) + fibonacciRecursive(number - 2);
 };
 
-console.log(fibonacciRecursive(13));
+console.log(fibonacciRecursive(10));
+
+function fibonacciMemoization() { //O(n)
+  let cache = {};
+  return function fib(n) {
+    calculations++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n-1) + fib(n-2);
+        return cache[n];
+      }
+    }
+  }
+};
+
+function fibonacciMemoization2(n) {
+  let answer = [0,1];
+  for ( let i = 2; i <= n; i++) {
+    answer.push(answer[i-2]+ answer[i-1]);
+  }
+  return answer.pop();
+}
+
+const fasterFib = fibonacciMemoization();
+
+console.log('DP', fasterFib(100));
+console.log('DP2', fibonacciMemoization2(100));
